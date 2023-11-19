d2199035138cd022c9f85a280d57989a77e6a9e1
Alena Hertman <gertmanea@mail.ru>
2023-11-19 16:07:07 +0300

    "перезалила"

--- src/main/java/HomeMavenHolla/Main.java
+++ src/main/java/HomeMavenHolla/Main.java
@@ -2,6 +2,18 @@ package HomeMavenHolla;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Players player1 = new Players("Вася", false);
+        long victoriesOld = Game.checkOldChoice(1000);
+        System.out.printf("%s выиграл %d раз", player1, victoriesOld);
+
+        System.out.println();
+        System.out.println("----------------");
+        Players player2 = new Players("Алёша", true);
+        long victoriesNew = Game.checkNewChoice(1000);
+        System.out.printf("%s выиграл %d раз", player2, victoriesNew);
+
+
+
     }
 }
